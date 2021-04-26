6a9f6c8ffa7b681ebde7593c75a4dce3a29ca3b6
Diptiman1999 <diptiman.sen1999@gmail.com>
2021-04-26 22:27:38 +0530

    print functions

---
+++ print.py
@@ -0,0 +1,13 @@
+print("\\")
+# To print backslash we need to print it twice
+
+# to Print sentences more than once
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+# to print in same line
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+# sep will join all string separated by comma and insert sep in between
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
